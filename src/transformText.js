37158bb4aea54fdfecbd120d15a9ddc3d48a0c53
Jay Hutchinson <r.jay.hutchinson@gmail.com>
2023-06-09 21:26:43 -0600

    improved random decisions

--- src/transformText.js
+++ src/transformText.js
@@ -16,16 +16,18 @@ export default function transformText(input, options) {
   	let prevRands = [];
     for (let i = 0; i < input.length; i++) {
       let c = input[i];
-      if (c == "I") {
-      	output += getChar(options, c.toLowerCase())
+      if (c === "I" && options.radio1 === "normal") {
+      	output += getChar(options, c.toLowerCase());
       	prevRands.unshift(Math.random() / 2);
-      } else if (c == "L") {
-      	output += getChar(options, c
+      } else if (c === "L" && options.radio1 === "normal") {
+      	output += getChar(options, c);
       	prevRands.unshift(Math.random() / 2 + 0.5);
       } else if (c >= "A" && c <= "Z") {
       	let myRand = Math.random();
-      	let myBias = prevRands.length >= 3 ? (prevRands.slice(0, 3).reduce((a, b) => a + b, 0)) / prevRands.slice(0, 3).length : 0.5;
-        output += getChar(options, myRand > myBias ? c : c.toLowerCase());
+      	let myBias = prevRands.length >= 1 ? (prevRands.slice(0, 3).reduce((a, b) => a + b, 0)) / prevRands.slice(0, 3).length : 0.5;
+      	let myChoice = myRand > myBias;
+        output += getChar(options, myChoice ? c : c.toLowerCase());
+        myRand += myChoice ? (1 - myRand) / 2 : myRand / -2;
         prevRands.unshift(myRand);
       } else {
         output += c;
